042769ed76801b4a530583d91068ff5cc4924729
trunghi1412 <91446424+trunghi1412@users.noreply.github.com>
2022-11-16 00:38:25 +0700

    Create useOnline.js

---
+++ Ex 12/useOnline.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+
+const useOnline = () => {
+    const [online, setOnline] = useState(navigator.online)
+
+    useEffect(() => {
+        if (window.addEventListener) {
+            window.addEventListener('online', () => setOnline(true), false)
+            window.addEventListener(
+                'offline',
+                () => setOnline(false),
+                false
+            )
+        } else {
+            document.body.ononline = () => setOnline(true)
+            document.body.onoffline = () => setOnline(false)
+
+        }
+    }, [])
+    return online
+}
+
+export default useOnline
